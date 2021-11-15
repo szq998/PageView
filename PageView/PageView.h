5088104c6fbbbee94fc8b4e03ae3fbdccd7fab2e
szq <szq001@foxmail.com>
2021-11-15 22:25:30 +0800

    Initial Commit

---
+++ PageView/PageView.h
@@ -0,0 +1,18 @@
+//
+//  PageView.h
+//  PageView
+//
+//  Created by szq on 2021/11/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PageView.
+FOUNDATION_EXPORT double PageViewVersionNumber;
+
+//! Project version string for PageView.
+FOUNDATION_EXPORT const unsigned char PageViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageView/PublicHeader.h>
+
+
